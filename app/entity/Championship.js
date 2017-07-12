128fdabd2876ae7896a28e6c46c8a6b8aa13361a
Alisson da Silva <alisson.silva@gmail.com>
2017-07-12 17:50:23 -0300

    fix nested

--- app/entity/Championship.js
+++ app/entity/Championship.js
@@ -41,69 +41,73 @@ export default class Championship extends BaseModel {
   }
 
   fetchById( id ) {
+
     const query = this.qb
-    .from('matches')
+    .from('championships')
     .select('*')
-    .leftJoin('rounds', 'matches.round_id', 'rounds.id')
-    .leftJoin('championships', 'rounds.championship_id', 'championships.id')
+    .leftJoin('rounds', 'rounds.championship_id', 'championships.id')
+    .leftJoin('matches', 'matches.round_id', 'rounds.id')
     .leftJoin('teams as team1', 'matches.team1', 'team1.id')
     .leftJoin('teams as team2', 'matches.team2', 'team2.id')
-    .where('rounds.date', '<', this.db.fn.now())
-    .andWhere('rounds.date_limit', '>', this.db.fn.now())
     .andWhere('championships.id', id)
-    .orderBy('matches.date')
+    //.orderBy('matches.date')
     .options({nestTables: true, rowMode: 'array'})
 
     let custom = {}
 
-    return query.then( res => {
+    return super.fetchById( id ).
+    then((championship) => {
+      return query.then( res => {
 
-      if(res.length > 0) {
-          custom = res[0].championships
-          custom.rounds = []
+        if(res.length > 0) {
+            custom = championship
+            custom.rounds = []
 
-          // filter rounds
-          res.forEach(item => {
+            // filter rounds
+            res.forEach(item => {
 
-            item.rounds.matches = []
+              item.rounds.matches = []
 
-            if(!custom.rounds.length) {
-              custom.rounds.push(item.rounds)
-            } else {
-              if(custom.rounds[custom.rounds.length-1].id != item.rounds.id) {
-                custom.rounds.push(item.rounds)
-              }
-            }
-
-          })
-
-          // filter matches
-          custom.rounds.forEach( item => {
-
-            let { id } = item
-
-            res.forEach((sms) => {
-              if(sms.matches.round_id = id) {
-
-                sms.matches.team1 = sms.team1
-                sms.matches.team2 = sms.team2
-
-                if(item.matches) {
-                  item.matches.push( sms.matches )
-                } else {
-                  item.matches = [ sms.matches ]
+              if(!custom.rounds.length) {
+                if( item.rounds.id )
+                  custom.rounds.push(item.rounds)
+              } else {
+                if(item.rounds.id && custom.rounds[custom.rounds.length-1].id != item.rounds.id) {
+                  custom.rounds.push(item.rounds)
                 }
               }
+
             })
 
+            // filter matches
+            custom.rounds.forEach( item => {
 
-          }) // endForeach
+              let { id } = item
+
+              res.forEach((sms) => {
+                if(sms.matches.round_id = id) {
+
+                  sms.matches.team1 = sms.team1
+                  sms.matches.team2 = sms.team2
+
+                  if(item.matches) {
+                    item.matches.push( sms.matches )
+                  } else {
+                    item.matches = [ sms.matches ]
+                  }
+                }
+              })
+
+
+            }) // endForeach
 
 
 
-      }
+        }
 
-      return custom
+        return custom
+
+      }) // nested query
 
     })
 
